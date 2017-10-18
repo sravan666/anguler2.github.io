bfef2bfe0a2ccf6fcc5e18a91cc9e4a72548c461
sravan reddy <singireddy.sravan@gmail.com>
2017-10-18 16:07:41 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: 'app/app.component.html' ,
+ 
+  /*
+                  //INLINE TEMPLATE
+  template: `<div>
+  <h1>{{appTitle}}</h1>
+  <div>Sravan reddy started anguler 2</div>
+</div>`,
+*/
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+  appTitle: string = 'Reddy';
+}
+
+
